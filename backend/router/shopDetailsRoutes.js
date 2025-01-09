d434d9a30f40a3188dfd0dd66c6fd60513e8bbfd
tharukamaduwantha <tharukamaduwantha62@gmail.com>
2025-01-09 22:13:08 +0530

    Map shop details with sorting filter

--- backend/router/shopDetailsRoutes.js
+++ backend/router/shopDetailsRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createShopDetails } from '../controllers/shopController.js';
+import { createShopDetails, getShopDetails } from '../controllers/shopController.js';
 
 const router = express.Router();
 
 // Define the POST route for creating shop details
 router.post('/', createShopDetails);
+
+// Define the GET route for fetching all shop details
+router.get('/', getShopDetails);
 
 export default router;
