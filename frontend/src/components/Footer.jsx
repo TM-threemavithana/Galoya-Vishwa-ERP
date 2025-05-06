da2725d2acf5f1bfb777640053b75c3505c12d07
Wasanjith Kodikara <149646160+wasanjith@users.noreply.github.com>
2025-05-06 12:13:55 +0530

    Change Fotter Color

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,11 +3,11 @@ import logo from "../assets/logo.png"; // Importing logo from assets folder
 
 const Footer = () => {
   return (
-    <div className="bg-[#DFF2EB]">
-      <div className="flex flex-col sm:grid sm:grid-cols-[3fr_1fr_1fr] gap-10 my-5 mt-20 text-sm px-5"> {/* Reduced gap, margin, and padding */}
+    <div className="bg-gradient-to-r from-green-900 via-green-600 to-green-300"> {/* Updated to gradient */}
+      <div className="flex flex-col sm:grid sm:grid-cols-[3fr_1fr_1fr] gap-10 my-5 mt-20 text-sm px-5">
         <div>
-          <img src={logo} className="mb-3 w-24" alt="Company Logo" /> {/* Reduced margin-bottom and width */}
-          <p className="w-full md:w-2/3 text-gray-600">
+          <img src={logo} className="mb-3 w-24" alt="Company Logo" />
+          <p className="w-full md:w-2/3 text-gray-100">
             Galoya Wishwa Dairy Products is committed to providing the highest
             quality dairy products to our customers. Our mission is to ensure
             freshness and nutritional value in every product we deliver. We take
@@ -17,8 +17,8 @@ const Footer = () => {
           </p>
         </div>
         <div className="mt-5">
-          <p className="text-xl font-medium mb-3">COMPANY</p>
-          <ul className="flex flex-col gap-1 text-gray-600">
+          <p className="text-xl font-medium mb-3 text-white">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-100">
             <li>No 23/131,</li>
             <li>Nawagampura Road,</li>
             <li>Uhana,</li>
@@ -26,16 +26,16 @@ const Footer = () => {
           </ul>
         </div>
         <div className="mt-5">
-          <p className="text-xl font-medium mb-3">GET IN TOUCH</p>
-          <ul className="flex flex-col gap-1 text-gray-600">
+          <p className="text-xl font-medium mb-3 text-white">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-100">
             <li>+94774081153</li>
-            <li>wishwadairy@gmail.com</li>
+            <li>Galoyavishwa@Yahoo.com</li>
           </ul>
         </div>
       </div>
       <div className="container mx-auto">
-        <hr />
-        <p className="py-3 text-sm text-center text-gray-600"> {/* Reduced padding */}
+        <hr className="border-gray-300" />
+        <p className="py-3 text-sm text-center text-gray-100">
           Copyright 2024@ Galoya Wishwa Products - All Right
         </p>
       </div>
